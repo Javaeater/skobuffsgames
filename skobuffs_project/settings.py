4c38f62acaf4d1ce18b7149e5a5ce25a74348141
Cesser Jackson <42046561+Javaeater@users.noreply.github.com>
2021-12-20 03:13:38 -0700

    Update settings.py

--- skobuffs_project/settings.py
+++ skobuffs_project/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-732@1$#zzjbqr-^oi=0m59fv766598=b#=c&giga!o!q2^$6^@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
